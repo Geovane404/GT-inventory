a13d21267b8a8a5510f890571961c1a5f9940519
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-08-19 09:07:57 -0300

    Category entity and CategoryController

---
+++ backend/src/main/java/com/gtecnologia/GTinventory/controllers/CategoryController.java
@@ -0,0 +1,27 @@
+package com.gtecnologia.GTinventory.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gtecnologia.GTinventory.entities.Category;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryController {
+	
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(2L, "Eletronics"));
+		list.add(new Category(3L, "Computers"));
+		
+		return ResponseEntity.ok().body(list);
+	}
+}
