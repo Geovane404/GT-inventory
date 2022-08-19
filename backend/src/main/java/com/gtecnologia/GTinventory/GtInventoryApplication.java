bcb013e6ac21b8f1b557bfe6e4353fd48f068207
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-08-19 08:16:54 -0300

    Backend project Created

---
+++ backend/src/main/java/com/gtecnologia/GTinventory/GtInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.GTinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtInventoryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gtecnologia/GTinventory/GtInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtecnologia.GTinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
