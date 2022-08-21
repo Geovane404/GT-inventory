92c7579940cbb75db10395c9e93e8851a9d8b49c
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-08-21 20:45:16 -0300

    CategoryDTO

--- backend/src/main/java/com/gtecnologia/GTinventory/controllers/CategoryController.java
+++ backend/src/main/java/com/gtecnologia/GTinventory/controllers/CategoryController.java
@@ -1,3 +1,4 @@
+
 package com.gtecnologia.GTinventory.controllers;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gtecnologia.GTinventory.dtos.CategoryDTO;
 import com.gtecnologia.GTinventory.entities.Category;
 import com.gtecnologia.GTinventory.services.CategoryService;
 
@@ -19,10 +21,9 @@ public class CategoryController {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 
-		List<Category> list = service.findAll();
-
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
--- backend/src/main/java/com/gtecnologia/GTinventory/services/CategoryService.java
+++ backend/src/main/java/com/gtecnologia/GTinventory/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.gtecnologia.GTinventory.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gtecnologia.GTinventory.dtos.CategoryDTO;
 import com.gtecnologia.GTinventory.entities.Category;
 import com.gtecnologia.GTinventory.repositories.CategoryRepository;
 
@@ -16,9 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
