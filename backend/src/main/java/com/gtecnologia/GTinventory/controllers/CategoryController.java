e14bf9b47c0d0178a0743646c77ef167584eedbc
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-08-24 16:33:51 -0300

    Created new category

--- backend/src/main/java/com/gtecnologia/GTinventory/controllers/CategoryController.java
+++ backend/src/main/java/com/gtecnologia/GTinventory/controllers/CategoryController.java
@@ -1,6 +1,7 @@
 
 package com.gtecnologia.GTinventory.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gtecnologia.GTinventory.dtos.CategoryDTO;
 import com.gtecnologia.GTinventory.services.CategoryService;
@@ -41,6 +45,15 @@ public class CategoryController {
 
 		CategoryDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
