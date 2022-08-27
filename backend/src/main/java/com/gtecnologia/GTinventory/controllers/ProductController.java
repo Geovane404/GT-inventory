fed28c9e92872d1a66451f8e26e9add63d30ada6
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-08-27 10:09:34 -0300

    Product findAllPaged, FindById and exception handling

--- backend/src/main/java/com/gtecnologia/GTinventory/controllers/ProductController.java
+++ backend/src/main/java/com/gtecnologia/GTinventory/controllers/ProductController.java
@@ -4,12 +4,16 @@ package com.gtecnologia.GTinventory.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gtecnologia.GTinventory.dtos.ProductDTO;
+import com.gtecnologia.GTinventory.entities.Product;
 import com.gtecnologia.GTinventory.services.ProductService;
 
 @RestController
@@ -24,6 +28,20 @@ public class ProductController {
 
 		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/paged")
+	public ResponseEntity<Page<ProductDTO>> findAllPaged(Pageable pageable) {
+
+		Page<ProductDTO> page = service.findAllPaged(pageable);
+		return ResponseEntity.ok().body(page);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+
+		ProductDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
--- backend/src/main/java/com/gtecnologia/GTinventory/services/ProductService.java
+++ backend/src/main/java/com/gtecnologia/GTinventory/services/ProductService.java
@@ -1,15 +1,19 @@
 package com.gtecnologia.GTinventory.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gtecnologia.GTinventory.dtos.ProductDTO;
 import com.gtecnologia.GTinventory.entities.Product;
 import com.gtecnologia.GTinventory.repositories.ProductRepository;
+import com.gtecnologia.GTinventory.services.exception.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -22,6 +26,21 @@ public class ProductService {
 
 		List<Product> list = repository.findAll();
 		return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
+
+		Page<Product> page = repository.findAll(pageable);
+		return page.map(x -> new ProductDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+
+		Optional<Product> obj = repository.findById(id);
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Id não encontrado!"));
+		return new ProductDTO(entity);
 	}
 
 }
