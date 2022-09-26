ff45b03a77deec95a7c062b0cfb9a09bfb8b8a17
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-09-26 17:58:31 -0300

    ProductControllerIT - Insert

--- backend/src/test/java/com/gtecnologia/GTinventory/controllers/ProductControllerIT.java
+++ backend/src/test/java/com/gtecnologia/GTinventory/controllers/ProductControllerIT.java
@@ -1,6 +1,7 @@
 package com.gtecnologia.GTinventory.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -14,6 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gtecnologia.GTinventory.dtos.ProductDTO;
+import com.gtecnologia.GTinventory.factory.Factory;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
@@ -22,9 +27,14 @@ public class ProductControllerIT {
 	@Autowired
 	private MockMvc mockMvc;
 	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
 	private long countTotalProduct;
 	private long existingid;
 	private long nonExistingId;
+	
+	private ProductDTO productDTO;
 	
 	@BeforeEach
 	void setUp() throws Exception{
@@ -32,6 +42,8 @@ public class ProductControllerIT {
 		countTotalProduct = 25L;
 		existingid = 1L;
 		nonExistingId = 1000L;
+		
+		productDTO = Factory.createProductDTO();
 	}
 	
 	
@@ -87,5 +99,24 @@ public class ProductControllerIT {
 				.accept(MediaType.APPLICATION_JSON));
 		
 		result.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void insertShouldReturnStatusCreatedAndProductDTO() throws Exception {
+		
+		String jsonBody = objectMapper.writeValueAsString(productDTO);
+		
+		ResultActions result = mockMvc.perform(post("/products/")
+				.content(jsonBody)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON));
+		
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.id").exists());
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.description").exists());
+		result.andExpect(jsonPath("$.price").exists());
+		result.andExpect(jsonPath("$.imgUrl").exists());
+		result.andExpect(jsonPath("$.date").exists());
 	}
 }
