533320beb5fb519dd9080d8bbbe19e8365e3b773
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-09-27 16:02:53 -0300

    UserRepository, UserDTO and RoleDTO

--- backend/src/main/java/com/gtecnologia/GTinventory/dtos/UserDTO.java
+++ backend/src/main/java/com/gtecnologia/GTinventory/dtos/UserDTO.java
@@ -1,49 +1,39 @@
-package com.gtecnologia.GTinventory.entities;
+package com.gtecnologia.GTinventory.dtos;
 
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import com.gtecnologia.GTinventory.entities.User;
 
-@Entity
-@Table(name = "tb_user")
-public class User implements Serializable{
+public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private Long id;
-	
 	private String firstName;
 	private String lastName;
 	private String email;
-	private String password;
 
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_user_role",
-			joinColumns = @JoinColumn(name = "user_id"),
-			inverseJoinColumns = @JoinColumn(name = "role_id"))
-	private Set<Role> roles = new HashSet<>();
+	private Set<RoleDTO> roles = new HashSet<>();
 
-	public User() {
+	public UserDTO() {
 
 	}
 
-	public User(Long id, String firstName, String lastName, String email, String password) {
+	public UserDTO(Long id, String firstName, String lastName, String email) {
 		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.password = password;
+	}
+
+	public UserDTO(User entity) {
+		id = entity.getId();
+		firstName = entity.getFirstName();
+		lastName = entity.getLastName();
+		email = entity.getEmail();
+
+		entity.getRoles().forEach(role -> this.roles.add(new RoleDTO(role)));
 	}
 
 	public Long getId() {
@@ -78,15 +68,7 @@ public class User implements Serializable{
 		this.email = email;
 	}
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Set<Role> getRoles() {
+	public Set<RoleDTO> getRoles() {
 		return roles;
 	}
 
@@ -106,7 +88,7 @@ public class User implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		UserDTO other = (UserDTO) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
---
+++ backend/src/main/java/com/gtecnologia/GTinventory/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gtecnologia.GTinventory.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gtecnologia.GTinventory.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
