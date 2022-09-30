0bae9347b74af0add92ed3f9501bc71387c33cf6
Geovane Calheira da Silva <geovanecalheiro@gmail.com>
2022-09-30 05:04:09 -0300

    Custom Validation Response

--- backend/src/main/java/com/gtecnologia/GTinventory/controllers/exception/ResourceExceptionHandler.java
+++ backend/src/main/java/com/gtecnologia/GTinventory/controllers/exception/ResourceExceptionHandler.java
@@ -4,8 +4,11 @@ import java.time.Instant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -41,6 +44,26 @@ public class ResourceExceptionHandler {
 		err.setError("Exceção do banco de dados");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(HttpServletRequest request, MethodArgumentNotValidException e) {
+
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+
+		err.setTimesTamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for(FieldError error : e.getBindingResult().getFieldErrors()) {
+			
+			err.addErrror(error.getField(), error.getDefaultMessage());
+		}
 
 		return ResponseEntity.status(status).body(err);
 	}
